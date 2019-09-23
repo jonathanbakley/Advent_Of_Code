091f49fda984d8555ae3d500ef7e4b99b6f240d5
Jonathan  Bakley <jonathanbakley@Jonathans-MacBook-Pro.local>
2019-09-22 20:40:14 -0400

    day 3 part 2 solution

--- 2015/day3solution.js
+++ 2015/day3solution.js
@@ -37,4 +37,56 @@ function dayThreeSolution(data) {
   return locations.length;
 }
 
-console.log(dayThreeSolution(data));
+function dayThreeSolutionPartTwo(data) {
+  const directions = data.split("");
+
+  const santaDirections = directions.filter((direction, i) => i % 2 === 0);
+  const roboSanataDirections = directions.filter((direction, i) => i % 2 === 1);
+
+  let santaLocations = getAllLocations(santaDirections);
+  let roboSanataLocations = getAllLocations(roboSanataDirections);
+
+  santaLocations = santaLocations.map(String);
+  roboSanataLocations = roboSanataLocations.map(String);
+
+  const allLocations = santaLocations.concat(roboSanataLocations);
+  const allUniqueLocations = allLocations.filter(
+    (location, i, ar) => ar.indexOf(location) === i
+  );
+
+  return allUniqueLocations.length;
+}
+
+function getAllLocations(directions) {
+  let currentLocation = [0, 0];
+  const locations = directions.reduce(
+    (locations, direction) => {
+      switch (direction) {
+        case ">":
+          currentLocation = [currentLocation[0] + 1, currentLocation[1]];
+          break;
+        case "<":
+          currentLocation = [currentLocation[0] - 1, currentLocation[1]];
+          break;
+        case "^":
+          currentLocation = [currentLocation[0], currentLocation[1] + 1];
+          break;
+        case "v":
+          currentLocation = [currentLocation[0], currentLocation[1] - 1];
+          break;
+      }
+      const stringLocations = JSON.stringify(locations);
+      const stringCurrent = JSON.stringify(currentLocation);
+      if (stringLocations.indexOf(stringCurrent) === -1) {
+        locations = locations.concat([currentLocation]);
+      }
+      return locations;
+    },
+    [[0, 0]]
+  );
+
+  return locations;
+}
+
+// console.log(dayThreeSolution(data));
+console.log(dayThreeSolutionPartTwo(data));
