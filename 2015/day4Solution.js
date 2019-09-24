51aa0f2f1e9f016636fc9f1f695f963c1c21f03d
jonathanbakley <jonathanbakley@gmail.com>
2019-09-24 19:22:58 -0400

    day 4 solution and day 5 setup

--- 2015/day4Solution.js
+++ 2015/day4Solution.js
@@ -1,14 +1,34 @@
 var input = "abcdef609043";
+var inputPuzzle = "iwrupvqb";
 var crypto = require("crypto");
 
 function dayFourSolution(input) {
-  // Add numbers starting with 1
-
-  const md5Solution = crypto
-    .createHash("md5")
-    .update(input)
-    .digest("hex");
-  console.log(md5Solution);
+  for (var i = 1; i < 990000; i++) {
+    testValue = input.concat(i.toString());
+    const md5Solution = crypto
+      .createHash("md5")
+      .update(testValue)
+      .digest("hex");
+    if (md5Solution.substring(0, 5) == "00000") {
+      console.log(md5Solution);
+      return i;
+    }
+  }
 }
 
-dayFourSolution(input);
+function dayFourSolutionPart2(input) {
+  for (var i = 1000000; i < 9999999; i++) {
+    testValue = input.concat(i.toString());
+    const md5Solution = crypto
+      .createHash("md5")
+      .update(testValue)
+      .digest("hex");
+    if (md5Solution.substring(0, 6) == "000000") {
+      console.log(md5Solution);
+      return i;
+    }
+  }
+}
+
+console.log(dayFourSolution(inputPuzzle));
+console.log(dayFourSolutionPart2(inputPuzzle));
