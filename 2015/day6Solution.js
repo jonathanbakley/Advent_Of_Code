9cd561d14824915f70fc877bbca1c94c988c126a
jonathanbakley <jonathanbakley@gmail.com>
2019-10-01 19:11:22 -0400

    still working on day 6

--- 2015/day6Solution.js
+++ 2015/day6Solution.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var path = require("path");
 
-const textFile = path.join(__dirname, "problemSixInputTEST.txt");
+const textFile = path.join(__dirname, "problemSixInput.txt");
 const data = fs.readFileSync(textFile, "utf8");
 
 function problemSixSolution(data) {
@@ -15,7 +15,7 @@ function problemSixSolution(data) {
       instructionArray[0] === "toggle"
         ? instructionArray[0]
         : instructionArray[1];
-    const startPosistion =
+    const startPosition =
       instructionArray[0] === "toggle"
         ? instructionArray[1]
         : instructionArray[2];
@@ -23,9 +23,12 @@ function problemSixSolution(data) {
       instructionArray[0] === "toggle"
         ? instructionArray[3]
         : instructionArray[4];
-    arrayOfEditedCells = getEditedGridCells(startPosistion, endPosition);
 
+    console.log("1. Break up instruction DONE");
+    arrayOfEditedCells = getEditedGridCells(startPosition, endPosition);
+    console.log("2. Get Edited Cells DONE");
     lightsGrid = updateLightsGrid(lightsGrid, arrayOfEditedCells, whatToDo);
+    console.log("3. updated lights grid DONE for step" + i);
   });
 
   let totalOn = 0;
@@ -35,7 +38,6 @@ function problemSixSolution(data) {
       totalOn = totalOn + 1;
     }
   });
-
   return totalOn;
 }
 
@@ -52,40 +54,32 @@ function getInitialGrid() {
 }
 
 function getEditedGridCells(start, end) {
-  console.log("start", start, end, "end");
-  const xAndYPositonEnd = end.split(",");
-  const xAndYPositonStart = start.split(",");
+  const xAndYPositionEnd = end.split(",");
+  const xAndYPositionStart = start.split(",");
 
   const EditedCells = [];
 
-  let xStart = xAndYPositonStart[0];
-  // console.log(xStart, "xStart", xAndYPositonStart[1], "yStart");
-  for (xStart; xStart <= xAndYPositonEnd[0]; xStart++) {
-    let yStart = xAndYPositonStart[1];
-    console.log(xStart, "xStart", yStart, "yStart");
-    for (yStart; yStart <= xAndYPositonEnd[1]; yStart++) {
-      console.log(xStart, "xStart", yStart, "yStart");
-      // FIX ISSUE HERE
+  const xEnd = parseInt(xAndYPositionEnd[0], 10);
+  const yEnd = parseInt(xAndYPositionEnd[1], 10);
+
+  let xStart = parseInt(xAndYPositionStart[0], 10);
+  for (xStart; xStart <= xEnd; xStart++) {
+    let yStart = parseInt(xAndYPositionStart[1], 10);
+    for (yStart; yStart <= yEnd; yStart++) {
       EditedCells.push(`${xStart},${yStart}`);
     }
   }
-  console.log("edited cells", EditedCells);
   return EditedCells;
 }
 
 function updateLightsGrid(lightsGrid, arrayOfEditedCells, whatToDo) {
-  // console.log(lightsGrid, arrayOfEditedCells, whatToDo);
-  console.log("IN FUNC", arrayOfEditedCells, whatToDo);
+  console.log("in updateLightGrid");
   const lightsGridUpdated = lightsGrid.map((light, i) => {
-    // console.log("light", light);
     if (arrayOfEditedCells.indexOf(light.position) != -1) {
-      console.log("light", light, whatToDo, arrayOfEditedCells);
       switch (whatToDo) {
         case "on":
-          // console.log("light on", light, whatToDo, { ...light, isOn: true });
           return { ...light, isOn: true };
         case "off":
-          // console.log("light off", light, whatToDo, { ...light, isOn: false });
           return { ...light, isOn: false };
         case "toggle":
           const isLightOn = !light.isOn;
@@ -94,7 +88,6 @@ function updateLightsGrid(lightsGrid, arrayOfEditedCells, whatToDo) {
     }
     return light;
   });
-  // console.log(lightsGridUpdated, "updated");
   return lightsGridUpdated;
 }
 
