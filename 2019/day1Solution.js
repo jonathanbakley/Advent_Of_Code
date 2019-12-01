720472eb173253fbaa5aee3bf8731b4fcbdea123
jonathanbakley <jonathanbakley@gmail.com>
2019-12-01 10:21:23 -0500

    small update

--- 2019/day1Solution.js
+++ 2019/day1Solution.js
@@ -5,14 +5,16 @@ const data = fs.readFileSync("day1Input.txt", "utf8");
 
 const modules = data.split("\n");
 
-const total = modules.reduce((total, singleModule) => {
-  let remander = Math.floor(singleModule / 3) - 2;
-  let moduleTotal = 0;
-  while (remander > 0) {
-    moduleTotal = moduleTotal + remander;
-    remander = Math.floor(remander / 3) - 2;
-  }
-  return total + moduleTotal;
-}, 0);
+function day1Solution(modules) {
+  return modules.reduce((total, singleModule) => {
+    let remander = Math.floor(singleModule / 3) - 2;
+    let moduleTotal = 0;
+    while (remander > 0) {
+      moduleTotal = moduleTotal + remander;
+      remander = Math.floor(remander / 3) - 2;
+    }
+    return total + moduleTotal;
+  }, 0);
+}
 
-console.log("Day 1 Solution: ", total);
+console.log("Day 1 Solution: ", day1Solution(modules));
